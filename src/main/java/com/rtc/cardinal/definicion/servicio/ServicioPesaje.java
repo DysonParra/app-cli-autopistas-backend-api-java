1493713ddc03bada219e84f1c4f73dea2d311ced
Dyson <dysontilano@gmail.com>
2022-10-11 11:50:58 -0500

    Updated license of '*.java' files

--- src/main/java/com/rtc/cardinal/definicion/servicio/ServicioPesaje.java
+++ src/main/java/com/rtc/cardinal/definicion/servicio/ServicioPesaje.java
@@ -1,13 +1,13 @@
 /*
- * @fileoverview {FileName} se encarga de realizar tareas especificas.
+ * @fileoverview {ServicioPesaje} se encarga de realizar tareas especificas.
  *
  * @version             1.0
  *
  * @author              Dyson Arley Parra Tilano <dysontilano@gmail.com>
  * Copyright (C) Dyson Parra
  *
- * @History v1.0 --- La implementacion de {FileName} fue realizada el 31/07/2022.
- * @Dev - La primera version de {FileName} fue escrita por Dyson A. Parra T.
+ * @History v1.0 --- La implementacion de {ServicioPesaje} fue realizada el 31/07/2022.
+ * @Dev - La primera version de {ServicioPesaje} fue escrita por Dyson A. Parra T.
  */
 package com.rtc.cardinal.definicion.servicio;
 
--- src/main/java/com/rtc/cardinal/definicion/web/rest/RecursoVehiculoSobrepeso.java
+++ src/main/java/com/rtc/cardinal/definicion/web/rest/RecursoVehiculoSobrepeso.java
@@ -1,13 +1,13 @@
 /*
- * @fileoverview {FileName} se encarga de realizar tareas especificas.
+ * @fileoverview {RecursoVehiculoSobrepeso} se encarga de realizar tareas especificas.
  *
  * @version             1.0
  *
  * @author              Dyson Arley Parra Tilano <dysontilano@gmail.com>
  * Copyright (C) Dyson Parra
  *
- * @History v1.0 --- La implementacion de {FileName} fue realizada el 31/07/2022.
- * @Dev - La primera version de {FileName} fue escrita por Dyson A. Parra T.
+ * @History v1.0 --- La implementacion de {RecursoVehiculoSobrepeso} fue realizada el 31/07/2022.
+ * @Dev - La primera version de {RecursoVehiculoSobrepeso} fue escrita por Dyson A. Parra T.
  */
 package com.rtc.cardinal.definicion.web.rest;
 
